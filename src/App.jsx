d192c7bbb2e11ebad88f9aeaf803a29fd1178b4b
v1ron <mindedbusines@gmail.com>
2025-06-24 01:42:07 +0000

    deleted two prblems

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import './darkmode.css'
 import Nav from './component/nav.jsx';
 import Intro from './component/intro.jsx';
 import About from './component/about.jsx';
@@ -9,7 +8,6 @@ import Skill from './component/skill.jsx';
 import Contact from './component/contact.jsx';
 import Last from './component/last.jsx';
 import ResumeButton from './component/ResumeButton.jsx';
-import DarkModeToggle from './DarkModeToggle.jsx';
 
 function App() {
   return (
